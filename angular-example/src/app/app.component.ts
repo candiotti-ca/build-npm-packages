310587332aea7b9d743060b6b66ae41a08d0d7d3
Carla Candiotti <carla.candiotti@hotmail.fr>
2024-02-08 10:43:26 +0100

    first commit

---
+++ angular-example/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class AppComponent {
+  title = 'angular-example';
+}
---
+++ vuejs-example/src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import {HelloWorld} from './index'
+</script>
+
+<template>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  <HelloWorld msg="Vue component" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
